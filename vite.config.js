b64e5a7dd73b4cc283f731a12080ef8bcb27a856
Srivaatsavreddy Panyam <srivaatsavreddy45@gmail.com>
2026-06-04 17:53:37 +0530

    Initail commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Studysync/', 
+  plugins: [react()],
+  build: {
+    outDir: 'docs',  
+  },
+  server: {
+    allowedHosts: 'all',
+  },
+})
