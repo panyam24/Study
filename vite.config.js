c994b6ea78fce0d5ec9d5ea8d49682ede758a45d
Srivaatsavreddy Panyam <srivaatsavreddy45@gmail.com>
2026-06-06 18:27:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Studysync/', 
   plugins: [react()],
-  build: {
-    outDir: 'docs',  
-  },
-  server: {
-    allowedHosts: true
-  },
+  
 })
